4411b5bd53f16323e91d0c38d642b7acc228791c
Giovanni <gilacqua@u.rochester.edu>
2018-12-09 18:54:44 -0500

    Attempted To Fix Matrix Factorization
    Ratings data is merged with movies to show the complete data

--- recommender_system/user_based.py
+++ recommender_system/user_based.py
@@ -1,3 +1,6 @@
+#Helpful Resource
+#https://towardsdatascience.com/how-did-we-build-book-recommender-systems-in-an-hour-part-2-k-nearest-neighbors-and-matrix-c04b3c2ef55c
+
 import pandas as pd # pandas is a data manipulation library
 import numpy as np #provides numerical arrays and functions to manipulate the arrays efficiently
 from scipy.sparse.linalg import svds #package to do single value decomposition
@@ -24,20 +27,35 @@ def moviesNotRatedByUser(userId):
 #User Based#
 ############
 
+def set_pandas_options() -> None:
+    pd.options.display.max_columns = 1000
+    pd.options.display.max_rows = 1000
+    pd.options.display.max_colwidth = 199
+    pd.options.display.width = None
+    # pd.options.display.precision = 2  # set as needed
+
+set_pandas_options()
+
+
 #Read file
 movies_data = pd.read_csv('ml-latest-small/movies.csv', sep=',')
 ratings_data=pd.read_csv('ml-latest-small/ratings.csv',sep=',')
 
-# print(movies_data.head())
+print(movies_data.head())
 #print(ratings_data.head())
 
 # remove timestamp column
 ratings_data = ratings_data.drop(["timestamp"], axis = 1)
+print(ratings_data.head(20))
 
+combined = ratings_data.merge(movies_data, left_on = "movieId", right_on = "movieId", how = "left")
+print(combined.head(20))
+
+ratings_data = combined
 #pivot ratings around userId
-# rating_pivot = ratings_data.pivot(index = 'userId', columns ='movieId', values = 'rating').fillna(0)
+#rating_pivot = ratings_data.pivot(index = 'userId', columns ='movieId', values = 'rating').fillna(0)
 rating_pivot = ratings_data.pivot(index = 'movieId', columns ='userId', values = 'rating').fillna(0)
-#print(rating_pivot)
+
 
 #transfrom into a sparse matrix for more efficient calculations
 rating_matrix = csr_matrix(rating_pivot.values)
@@ -46,15 +64,15 @@ model_knn.fit(rating_matrix)
 
 #query_index = np.random.choice(rating_pivot.shape[0])
 query_index = 1
-print(rating_pivot.index[query_index])
+#print(rating_pivot.index[query_index])
 
 distances, indices = model_knn.kneighbors(rating_pivot.iloc[query_index, :].reshape(1, -1), n_neighbors = 6)
 
 for i in range(0, len(distances.flatten())):
     if i == 0:
-        print("Recommendations for {0}:\n".format(rating_pivot[query_index]))
+        print("Recommendations for {0}:\n".format(rating_pivot.index[query_index]))
     else:
-        print("{0}: {1}, width distance of {2}:".format(i, rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+        print("{0}: {1}, with distance of {2}:".format(i, rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
 
 
 
